10b8e2cd10cdd6870079be865bf7f59927e3d02f
WieeRd <verywieerd@gmail.com>
2024-03-04 02:46:44 +0900

    feat: add OpenSSL AES-128 backend of `Encrypter`

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
+// DOC: add #![deny(missing_doc)] and start working on the documentation
+
 mod anonymize;
 // mod deanonymize;
+
+pub mod backends;
 
 pub use anonymize::{Anonymizer, Encrypter};
 // pub use deanonymize::{Deanonymizer, Decrypter};
